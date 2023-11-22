9810421b7494a5b8f61c6f47a29ba42172abbbd8
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-11-22 06:10:05 -0800

    arash
    add a first app

---
+++ mainweb/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+from mainweb.views import *
+
+urlpatterns = [
+ 
+    path('',index_views),
+]
--- website/urls.py
+++ website/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from website.view import http_test
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('http-test',http_test)
+    path('',include('mainweb.urls')),
 ]
