2dccd36b3d394bf4b4ac94a156d8510025a8e527
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-12-03 04:50:08 -0800

    arash
    add models

--- mainweb/models.py
+++ mainweb/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    phonenumber=models.IntegerField()
+    date=models.DateTimeField(auto_now_add=True)
+    meal=models.TextField()
+    capacity=models.TextField()
+
+
+
+
+
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
