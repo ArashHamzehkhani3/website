e82b80a7664fce4cead7832f566f27623f7b9893
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-12-04 01:16:01 -0800

    arash
    add function models

--- mainweb/admin.py
+++ mainweb/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mainweb.models import Book,Contact
+
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Contact)
+
--- mainweb/models.py
+++ mainweb/models.py
@@ -6,10 +6,14 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
-    phonenumber=models.IntegerField()
+    phonenumber=models.CharField(max_length=200)
     date=models.DateTimeField(auto_now_add=True)
-    meal=models.TextField()
-    capacity=models.TextField()
+    meal=models.CharField(max_length=100)
+    capacity=models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
 
 
 
