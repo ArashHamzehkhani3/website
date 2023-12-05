4f7df1025986f0b1a11c66169eb86329a008a9ab
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-12-05 05:17:36 -0800

    arash
    add class BookAdmin

--- mainweb/admin.py
+++ mainweb/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 from mainweb.models import Book,Contact
 
 # Register your models here.
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('name','meal','capacity')
+    ordering=['date']
+    search_fields=['name']
+
+
+
+
+
+
+
+admin.site.register(Book,BookAdmin)
 admin.site.register(Contact)
+
+
 
