cf6a878fe305dabef9a60269f0aa64bd7b0102ef
Arash Hamzehkhani <110529812+ArashHamzehkhani3@users.noreply.github.com>
2023-12-09 06:28:07 -0800

    arash
    connect form to model

--- mainweb/views.py
+++ mainweb/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from mainweb.models import Book
+from mainweb.models import Book,Contact
 
 
 def index_views(request):
@@ -15,7 +15,18 @@ def test_view(request):
       email=request.POST.get('email')
       subject=request.POST.get('subject')
       message=request.POST.get('message')
+      
+      c = Contact()
+
+      c.name=name
+      c.email=email
+      c.subject=subject
+      c.message=message
+
+      c.save()
       print(name,email,subject,message)
+
+
 
 
    return render(request,'test.html',{})
